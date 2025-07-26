9e22a57c30ce3b5afc945bfb90056bc44af360df
Bruno Ferreira de Oliveira <87425146+brunoferromega@users.noreply.github.com>
2025-07-26 13:17:35 +0000

    Third ex done - read from terminal

--- Ch02/02_03b/main.go
+++ Ch02/02_03b/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter a text: ")
+	str, _ := reader.ReadString('\n')
+	fmt.Println(str)
+
+	fmt.Print("Enter a float number: ")
+	str, _ = reader.ReadString('\n')
+	fNum, err := strconv.ParseFloat(strings.TrimSpace(str), 64)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Value of number: ", fNum)
 }
