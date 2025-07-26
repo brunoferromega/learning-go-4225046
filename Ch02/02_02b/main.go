e0c21d87a0688ab65f9caf683c0549fc99ca66a7
Bruno Ferreira de Oliveira <87425146+brunoferromega@users.noreply.github.com>
2025-07-26 13:02:17 +0000

    Done second ex - var and prints

--- Ch02/02_02b/main.go
+++ Ch02/02_02b/main.go
@@ -6,9 +6,16 @@ import (
 
 func main() {
 
-	// str1 := "The quick red fox"
-	// str2 := "jumped over"
-	// str3 := "the lazy brown dog."
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog."
+	iamNum := 23
 
-	fmt.Println("Hello from Go!")
+	fmt.Println(str1, str2, str3)
+	strSize, err := fmt.Println("I'm the number", iamNum)
+	if err == nil {
+		fmt.Printf("The string size is %v\n", strSize)
+	}
+
+	fmt.Printf("Checking a var type %T\n", iamNum)
 }
